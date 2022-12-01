8ae20a5464609ffddd86e5f5fd8d70dfd4506ea1
LamzinAV <lamzinav@gmail.com>
2022-12-01 19:16:53 +0300

    task

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Функция подсчета подходящих элементов
+int CountElement (string[] array)
+{
+    int size = 0, temp = 0;
+    string box = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        box = array[i];
+        temp = box.Length;
+        if (temp < 4)
+            size ++;
+    }
+    return size;
+}
+
+// Функция формирования нового массива с заданными элементами по условию задачи
+string[] ConvertArray (string[] array, int size)
+{
+    int j = 0, temp = 0;
+    string box = "";
+    string[] resultArray = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        box = array[i];
+        temp = box.Length;
+        if (temp < 4)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+// Код программы
+string[] originalArray = {"2323","big","ralli","//(","4<"};
+int sizeArray = CountElement(originalArray);
+string[] finalArray = new string[sizeArray];
+finalArray = ConvertArray(originalArray, sizeArray);
+Console.Write(string.Join(", ", finalArray));
